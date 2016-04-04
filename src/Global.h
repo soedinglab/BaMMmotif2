af0de1809fad757cacb24ed4c8eaf748fbb31a9e
Wanwan Ge <gewanwan92@gmail.com>
2016-04-04 16:54:32 +0200

    change several int variables to unsigned int

--- src/Global.h
+++ src/Global.h
@@ -9,6 +9,12 @@
 #define GLOBAL_H_
 
 #include "SequenceSet.h"
+#include "Alphabet.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <vector>
 
 class Global {
 
@@ -16,14 +22,14 @@ public:
 
 	static char* 			outputDirectory;        	// output directory
 
-	static char*			alphabetString;				// defaults to ACGT but may later be extended to ACGTH(ydroxymethylcytosine) or similar
 	static char* 			posSequenceFilename;		// filename of positive sequence fasta file
 	static char* 			negSequenceFilename;		// filename of negative sequence fasta file
 
+	static char*			alphabetString;				// defaults to ACGT but may later be extended to ACGTH(hydroxymethylcytosine) or similar
 	static bool				revcomp;					// also search on reverse complement of sequences
 
-	static SequenceSet* 	posSequenceSet;				// positive SequenceSet
-	static SequenceSet* 	negSequenceSet;				// negative SequenceSet
+	static SequenceSet* 	posSequenceSet;				// positive Sequence Set
+	static SequenceSet* 	negSequenceSet;				// negative Sequence Set
 
 	static char* 			intensityFilename;			// filename of intensity file (i.e. for HT-SELEX data)
 	// further weighting options...
@@ -35,33 +41,33 @@ public:
 	static char* 			iIMMFilename;				// filename of Markov model (.iimm) file
 
 	// model options
-	static int 				modelOrder;					// model order
+	static unsigned int 	modelOrder;					// model order
 	static float** 			modelAlpha;					// initial alphas
 	static std::vector<int> addColumns;					// add columns to the left and right of models used to initialize Markov models
 	static bool				noLengthOptimization;		// disable length optimization
 
 	// background model options
-	static int 				bgModelOrder;				// background model order
+	static unsigned int 	bgModelOrder;				// background model order, defaults to 2
 	static float** 			bgModelAlpha;				// background model alphas
 
 	// EM options
-	static int				maxEMIterations;			// maximum number of iterations
+	static unsigned int		maxEMIterations;			// maximum number of iterations
 	static float 			epsilon;					// likelihood convergence parameter
 
 	static bool				noAlphaOptimization;		// disable alpha optimization
 	static bool				noQOptimization;			// disable q optimization
 
 	// FDR options
-	static int 				M;							// number of negative sequences as multiple of positive sequences
-	static int 				nFolds;						// number of cross-validation folds
-	static std::vector<std::vector<int>> posFoldIndices;// sequence indices for each cross-validation fold
-	static std::vector<std::vector<int>> negFoldIndices;// sequence indices for each cross-validation fold
-
+	static bool				FDR;						// triggers False-Discovery-Rate (FDR) estimation
+	static unsigned int 	mFold;						// number of negative sequences as multiple of positive sequences
+	static unsigned int 	nFolds;						// number of cross-validation folds
+	static std::vector< std::vector<int> > posFoldIndices;// sequence indices for each cross-validation fold
+	static std::vector< std::vector<int> > negFoldIndices;// sequence indices for each cross-validation fold
 	// further FDR options...
 
 	static bool				verbose;					// verbose printouts
 
-	static void init( int nargs, char* args[] ){
+	static void init( int nargs, char *args[] ){
 		readArguments( nargs, args );
 		Alphabet::init( alphabetString );
 		// read in positive (and negative) sequence set
@@ -76,7 +82,7 @@ public:
 
 private:
 
-	static int  readArguments( int nargs, char *args[] );
+	static int readArguments( int nargs, char *args[] );
 	static void printHelp();
 
 	static void createDirectory();
