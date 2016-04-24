d85afb77808b02903ad65446d51143b76e2979ed
administrator <administrator@l1161-09>
2016-04-24 22:17:02 +0200

    test bash script

---
+++ build/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###
+### This is a test bash script for BaMMmotif_development
+###
+
+./BaMMmotif /home/administrator/benchmark/test/benchmark /home/administrator/benchmark/test/test.fasta
