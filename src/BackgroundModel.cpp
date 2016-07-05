c9a8796ceb69e9e3387ebcafe7eb69287a42be0a
Wanwan Ge <gewanwan92@gmail.com>
2016-07-05 13:36:24 +0200

    change extractKmer()

--- src/BackgroundModel.cpp
+++ src/BackgroundModel.cpp
@@ -9,17 +9,11 @@
 #include "SequenceSet.h"
 
 BackgroundModel::BackgroundModel(){
-	K_ = Global::bgModelOrder;
-
-	powA_ = new int[K_+2];
-	for( int k = 0; k < K_ + 2; k++ )
-		powA_[k] = Global::ipow( Alphabet::getSize(), k );
-
 	n_bg_ = ( int** )calloc( K_+1, sizeof( int* ) );
 	v_bg_ = ( float** )calloc( K_+1, sizeof( float* ) );
 	for( int k = 0; k < K_+1; k++ ){
-		n_bg_[k] = ( int* )calloc( powA_[k+1], sizeof( int ) );
-		v_bg_[k] = ( float* )calloc( powA_[k+1], sizeof( float ) );
+		n_bg_[k] = ( int* )calloc( Global::powA[k+1], sizeof( int ) );
+		v_bg_[k] = ( float* )calloc( Global::powA[k+1], sizeof( float ) );
 	}
 }
 
@@ -30,8 +24,6 @@ BackgroundModel::~BackgroundModel(){
 	}
 	free( v_bg_ );
 	free( n_bg_ );
-
-	delete[] powA_;
 }
 
 void BackgroundModel::init( std::vector<int> folds = std::vector<int> () ){
@@ -66,46 +58,45 @@ void BackgroundModel::init( std::vector<int> folds = std::vector<int> () ){
 		}
 	}
 
-	 // Only for testing
-	if( Global::verbose ){
-		printf( " ___________________________\n"
-				"|                           |\n"
-				"| k-mer counts for bgModel  |\n"
-				"|___________________________|\n\n" );
-		for( int k = 0; k < K_+1; k++ ){
-			for( int y = 0; y < powA_[k+1]; y++ )
-				std::cout << n_bg_[k][y] << '\t';
-			std::cout << std::endl;
-		}
-	}
+//	 // Only for testing
+//	if( Global::verbose ){
+//		printf( " ___________________________\n"
+//				"|                           |\n"
+//				"| k-mer counts for bgModel  |\n"
+//				"|___________________________|\n\n" );
+//		for( int k = 0; k < K_+1; k++ ){
+//			for( int y = 0; y < powA_[k+1]; y++ )
+//				std::cout << n_bg_[k][y] << '\t';
+//			std::cout << std::endl;
+//		}
+//	}
 
     // calculate v from k-mer counts n_occ
 	calculateV();
 
 	if( Global::verbose ) print();
-	write();
 }
 
 void BackgroundModel::calculateV(){
 
 	// sum over counts for all mono-nucleotides
 	float sum = 0.0f;
-	for( int y = 0; y < powA_[1]; y++ )
+	for( int y = 0; y < Global::powA[1]; y++ )
 		sum += float( n_bg_[0][y] );
 
 	// for k = 0: v = frequency
-	for( int y = 0; y < powA_[1]; y++ )
+	for( int y = 0; y < Global::powA[1]; y++ )
 		v_bg_[0][y] = n_bg_[0][y] / sum;
 
 	// for k > 0:
 	int y2;										// cut off the first nucleotide in (k+1)-mer y, e.g. from ACGT to CGT
 	int yk;										// cut off the last nucleotide in (k+1)-mer y , e.g. from ACGT to ACG
 	for( int k = 1; k < K_+1; k++ ){
-		for( int y = 0; y < powA_[k+1]; y++ ){
-			y2 = y % powA_[1];
-			yk = y / powA_[k];
-			v_bg_[k][y] = ( n_bg_[k][y] + Global::bgModelAlpha * v_bg_[k-1][y2] ) / ( n_bg_[k-1][yk] + Global::bgModelAlpha );
-			std::cout << std::fixed << std::setprecision(6) << v_bg_[k-1][y] << '\t' << n_bg_[k-1][yk] << std::endl;
+		for( int y = 0; y < Global::powA[k+1]; y++ ){
+			y2 = y % Global::powA[k];
+			yk = y / Global::powA[1];
+			v_bg_[k][y] = ( n_bg_[k][y] + Global::bgModelAlpha * v_bg_[k-1][y2] )
+						/ ( n_bg_[k-1][yk] + Global::bgModelAlpha );
 		}
 	}
 }
@@ -120,7 +111,7 @@ void BackgroundModel::print(){
 			"| probabilities for bgModel |\n"
 			"|___________________________|\n\n" );
 	for( int k = 0; k < K_+1; k++ ){
-		for( int y = 0; y < powA_[k+1]; y++ )
+		for( int y = 0; y < Global::powA[k+1]; y++ )
 			std::cout << std::fixed << std::setprecision(6) << v_bg_[k][y] << '\t';
 		std::cout << std::endl;
 	}
@@ -130,7 +121,7 @@ void BackgroundModel::write(){
 	std::string opath = std::string( Global::outputDirectory )  + '/' + std::string( Global::posSequenceBasename ) + ".condsBg";
 	std::ofstream ofile( opath.c_str() );
 	for( int k = 0; k < K_+1; k++ ){
-		for( int y = 0; y < powA_[k+1]; y++ )
+		for( int y = 0; y < Global::powA[k+1]; y++ )
 			ofile << std::fixed << std::setprecision(6) << v_bg_[k][y] << ' ';
 		ofile << std::endl;
 	}
