255656f1331e0bf177b1a7df3725ca3850e38379
Wanwan Ge <gewanwan92@gmail.com>
2016-07-05 13:37:30 +0200

    EM still does not work

--- src/EM.h
+++ src/EM.h
@@ -24,28 +24,27 @@ public:
 
 private:
 
-	Motif* 				motif_;			// motif to optimize within the EM
-	float**				v_bg_;			// conditional probabilities for background model
+	Motif* 				motif_;				// motif to optimize within the EM
+	float**				v_bg_;				// conditional probabilities for background model
 
-	std::vector<int>	folds_;		    // folds to iterate over, for cross-validation
+	std::vector<int>	folds_;		    	// folds to iterate over, for cross-validation
 
-	float**				s_;				// log odds scores of each (k+1)-mer at each position
-	float** 			r_;		        // responsibilities at position i in sequence n
-	float*** 			n_;	            // fractional counts n for (k+1)-mers y at motif position j
-	float** 			alpha_;	        // pseudo-count hyper-parameter for order k and motif position j
-	float 				q_; 			// hyper-parameter q specifies the fraction of sequences with motif
+	float**				s_;					// log odds scores of each (k+1)-mer at each position
+	float** 			r_;		        	// responsibilities at position i in sequence n
+	float*** 			n_;	            	// fractional counts n for (k+1)-mers y at motif position j
+	float** 			alpha_;	        	// pseudo-count hyper-parameter for order k and motif position j
+	float 				q_ = 0.9f; 			// hyper-parameter q specifies the fraction of sequences containing motif
 
-	float 				likelihood_;	// value of Q function for current parameters
-	unsigned int 		EMIterations_;  // counter for EM iterations => can be set as a local variable
-	int*				powA_;			// size of alphabet to the power k
+	float 				likelihood_ = 0;	// value of Q function for current parameters
+	unsigned int 		EMIterations_ = 0;  // counter for EM iterations => can be set as a local variable
 
-	void EStep();						// E-step
-	void MStep();						// M-step
+	void EStep();							// E-step
+	void MStep();							// M-step
 
-	void optimizeAlphas();			    // optimize alpha hyper-parameters
-	void optimizeQ();					// optimize hyper-parameter q
+	void optimizeAlphas();			    	// optimize alpha hyper-parameters
+	void optimizeQ();						// optimize hyper-parameter q
 
-//	float sumV( float*** v );			// sum up the conditional probabilities
+//	float sumV( float*** v );				// sum up the conditional probabilities
 };
 
 //inline float EM::sumV( float*** v ){
