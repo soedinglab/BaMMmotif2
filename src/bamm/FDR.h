2fa17938f128c0de81cda1ba3c51d44941b02246
anjakiesel <kiesel@genzentrum.lmu.de>
2017-01-20 16:36:59 +0100

    1.) removed exit on non-optimized motif so that one can run FDR on PWMs without running EM or CGS before 2.) changed FDR outputfile names by extending them with _motif_#_ so that when running FDR for more than one motif (i.e. peng-input) the output files do not get overwritten 3.) included output file posSequenceBasename.zoops.stats /*mops.stats which contain TP FP TN FN values. The function calculateTPFPFNTN should however be encapsuled within a parameter flag so that it is not automatically produced (will do this soon)

--- src/bamm/FDR.h
+++ src/bamm/FDR.h
@@ -19,8 +19,8 @@ public:
 	float 	getPrec_middle_ZOOPS();			// get precision when recall = 0.5 for ZOOPS model
 	float 	getPrec_middle_MOPS();			// get precision when recall = 0.5 for MOPS model
 	void 	print();
-	void 	write();
-	void 	writeLogOdds();					// print out log odds scores for both positive and negative set
+	void 	write(int n);
+	void 	writeLogOdds( int n);			// print out log odds scores for both positive and negative set
 											// for both MOPS and ZOOPS models
 
 private:
@@ -37,11 +37,20 @@ private:
 
 	std::vector<float>	Pre_ZOOPS_;			// precision for ZOOPS model
 	std::vector<float>	Rec_ZOOPS_;			// recall for ZOOPS model
+	std::vector<float>  ZOOPS_TP_;			// true positives for ZOOPS model
+	std::vector<float>  ZOOPS_FP_;			// false positives for ZOOPS model
+	std::vector<float>  ZOOPS_FN_;			// false negatives for ZOOPS model
+	std::vector<float>  ZOOPS_TN_;			// true negatives for ZOOPS model
 
 	std::vector<float>	Pre_MOPS_;			// precision for MOPS model
 	std::vector<float>	Rec_MOPS_;			// recall for MOPS model
 	std::vector<float>	FP_MOPS_;			// false positive values for MOPS model
 	std::vector<float>	TFP_MOPS_;			// true and false positive values for MOPS model
+	std::vector<float>  MOPS_TP_;			// true positives for MOPS model
+	std::vector<float>  MOPS_FP_;			// false positives for MOPS model
+	std::vector<float>  MOPS_FN_;			// false negatives for MOPS model
+	std::vector<float>  MOPS_TN_;			// true negatives for MOPS model
+
 
 	float 				prec_mid_ZOOPS_ = 0.0f;	// precision when recall = 0.5 for ZOOPS model
 	float 				prec_mid_MOPS_ = 0.0f;	// precision when recall = 0.5 for MOPS model
@@ -61,6 +70,9 @@ private:
 
 							// score sequences for both positive and negative sets
 	std::vector<std::vector<float>>	scoreSequenceSet( Motif* motif, BackgroundModel* bg, std::vector<Sequence*> seqSet );
+
+							// calculate TP FP FN and TN for ZOOPS and MOPS models
+	void 					caclulateTPFPFNTN();
 
 							// calculate precision and recall for both ZOOPS and MOPS models
 	void 		   			calculatePR();
