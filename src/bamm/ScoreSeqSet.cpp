56ca7fb805861f94572dd5baf342894191f93a27
anjakiesel <kiesel@genzentrum.lmu.de>
2017-04-04 14:41:47 +0200

    fix deletion of extractKmer fkt() in scoreseqset.cpp; adjust output format of scoreseqset_write()

--- src/bamm/ScoreSeqSet.cpp
+++ src/bamm/ScoreSeqSet.cpp
@@ -34,14 +34,12 @@ void ScoreSeqSet::score(){
 	int 	K = Global::modelOrder;
 	int 	K_bg = Global::bgModelOrder;
 	int 	W = motif_->getW();
-	float 	maxScore;								// maximal logOddsScore over all positions for each sequence
+	//float 	maxScore;								// maximal logOddsScore over all positions for each sequence
 
 	for( size_t n = 0; n < seqSet_.size(); n++ ){
 
 		int LW1 = seqSet_[n]->getL() - W + 1;
 		int* kmer = seqSet_[n]->getKmer();
-
-		//maxScore = -FLT_MAX;
 
 		std::vector<float> logOdds( LW1 );
 
@@ -51,13 +49,11 @@ void ScoreSeqSet::score(){
 
 			for( int j = 0; j < W; j++ ){
 
-				int y = kmer[i+j] % (i+j < K ) ? Y_[i+j+1] : Y_[K+1];
+				int y = kmer[i+j] % Y_[(i+j < K ) ? i+j+1 : K+1];
 				int y_bg = y % Y_[K_bg+1];
 
 				if( y >= 0 ){
-
 					logOdds[i] += ( logf( motif_->getV()[K][y][j] ) - logf( bg_->getV()[std::min( K, K_bg )][y_bg] ) );
-
 				}
 			}
 
@@ -85,6 +81,7 @@ void ScoreSeqSet::write(int N,float cutoff){
 	 */
 
 	bool first_hit = true;
+	int end; 				// end of motif match
 
 	std::string opath = std::string( Global::outputDirectory ) + '/'
 			+ Global::posSequenceBasename +  "_motif_" + std::to_string( N+1 ) + ".logOdds";
@@ -93,19 +90,30 @@ void ScoreSeqSet::write(int N,float cutoff){
 
 	for( size_t n = 0; n < seqSet_.size(); n++ ){
 		first_hit = true;
+		int seqlen = seqSet_[n]->getL();
+		if(Global::revcomp){
+			seqlen = seqlen/2;
+		}
 		int LW1 = seqSet_[n]->getL() - motif_->getW() + 1;
 		for( int i = 0; i < LW1; i++ ){
 			if ( scores_ [n][i]>cutoff ) {
 				if ( first_hit ){
-					ofile << seqSet_[n]->getHeader() << std::endl;
+					// >header:sequence_length
+					ofile << '>' << seqSet_[n]->getHeader() <<  ':' << seqlen << std::endl;
 					first_hit = false;
 				}
-				ofile << i << ':' << std::setprecision( 3 ) << scores_[n][i] << ' ';
+				// start:end:score:strand:sequence_matching
+				end = i + motif_->getW()-1;
+
+				ofile << i << ':' << end << ':' << std::setprecision( 3 ) << scores_[n][i] << ':' <<
+						((i < seqlen) ? '+' : '-') << ':' ;
+				for( int m=i; m<=end;m++){
+					ofile << Alphabet::getBase( seqSet_[n]->getSequence()[m] );
+				}
+				ofile << std::endl;
 			}
 		}
-		if( ! first_hit ){
-			ofile << std::endl;
-		}
+		//ofile << std::endl;
 	}
 
 }
