e2da780de6b39cfef2334d3895bd6bf75e4ba8d5
Wanwan Ge <gewanwan92@gmail.com>
2017-04-19 16:47:44 +0200

    add two flags: --mops and --plot to trigger the calculation for mops model and the plotting of FDR-recall curve(s)

--- R/plotAUSFC_rank.R
+++ R/plotAUSFC_rank.R
@@ -5,16 +5,18 @@
 #
 #-----------------------------------------------------------------
 
-# calculate FDR, precision, recall and occurrences for both MOPS
-# and ZOOPS models by taking p-values of log odds scores from
+# calculate FDR, precision, recall and occurrences for ZOOPS
+# and MOPS (optional) models by taking p-values of log odds scores from
 # positive sequences and using "fdrtool" library;
-# plot FDR vs. recall(sensitivity) curve;
-# caluclate the area under the sensitivity-FDR curve (AUSFC);
-# results are saved in a .rankscore file;
+# plot FDR vs. recall(sensitivity) curve (optional);
+# caluclate the area under the sensitivity-FDR curve (AUSFC).
 
 # examples for running this script:
-# ./plotAUSFC_benchmark_fdrtool.R PATH_TO_zoops.stats_FILE BASENAME_OF_THE_FILE PATH_OF_OUTPUT_FILE
+# ./plotAUSFC_benchmark_fdrtool.R PATH_TO_zoops.stats_FILE BASENAME_OF_THE_FILE PATH_OF_OUTPUT_FILE [--mops] [--plot]
 # ./plotAUSFC_benchmark_fdrtool.R /home/bamm_result/ JunD /home/bamm_result/output.txt
+
+# If --mops is applied, the mops.pvalue file is required.
+# If --plot is applied, a plot of FDR-recall curve will be generated.
 
 #-----------------------------
 #
@@ -40,6 +42,8 @@ parser <- ArgumentParser(description='benchmark motif finder')
 parser$add_argument('target_directory', help='directory that contains the target file')
 parser$add_argument('prefix', help='prefix of the target file')
 parser$add_argument('output_file', help='file to print the benchmark results')
+parser$add_argument('--mops', action="store_true", default='False')
+parser$add_argument('--plot', action="store_true", default='False')
 
 args <- parser$parse_args()
 
@@ -51,7 +55,11 @@ dir <- args$target_directory
 prefix <- args$prefix
 output <- args$output_file
 
-results = c(paste(c("prefix", "motifNumber", "ausfc_zoops*occ_frac", "occ_frac"), collapse="\t"))
+if( args$mops != 'False' ){ 
+  results = c(paste(c("prefix", "motifNumber", "ausfc_zoops*occ_frac", "occ_frac", "ausfc_mops*occ_mult", "occ_mult"), collapse="\t"))
+} else {
+  results = c(paste(c("prefix", "motifNumber", "ausfc_zoops*occ_frac", "occ_frac"), collapse="\t"))
+}
 for (f in Sys.glob(paste(c(dir, "/", prefix, "_motif_*", ".zoops.pvalues"), collapse=""))) {
   motifNumber <- sub(paste(c(dir, "/", prefix, "_motif_"), collapse=""), "", f)
   motifNumber <- sub(".zoops.pvalues", "", motifNumber)
@@ -134,18 +142,115 @@ for (f in Sys.glob(paste(c(dir, "/", prefix, "_motif_*", ".zoops.pvalues"), coll
     ausfc_zoops = sum(diff(log10(fdr_zoops[list_zoops]))*rollmean(recall_zoops[list_zoops],2)) / sum_area
   }
   # plot fdr vs. recall curve
-#  png( file = paste( dir, "/", prefix, "_motif_", motifNumber, '_SFCurve_ZOOPS.png', sep = "" ) )
-#  plot(fdr_zoops[list_zoops], recall_zoops[list_zoops], log="x",
-#       main=paste(prefix, "_motif_", motifNumber, " Sensitivity vs. FDR for ZOOPS model", sep=""),
-#       xlab="FDR", ylab="Sensitivity", xlim=c(0.0001,0.5), ylim=c(0,1),
-#       type='l', lwd=2.5,
-#       col="deepskyblue")
-#  text(0.0005, 0.95, paste( "AUSFC: ", round(ausfc_zoops, digits=4) ))
-#  text(0.0005, 0.85, paste( "Frac. Occ: ", round(occ_frac, digits=4) ))
-#  dev.off()
+
+  if( args$plot != 'False' ){
+     png( file = paste( dir, "/", prefix, "_motif_", motifNumber, '_SFCurve_ZOOPS.png', sep = "" ) )
+     plot(fdr_zoops[list_zoops], recall_zoops[list_zoops], log="x",
+          main=paste(prefix, "_motif_", motifNumber, " Sensitivity vs. FDR for ZOOPS model", sep=""),
+          xlab="FDR", ylab="Sensitivity", xlim=c(0.0001,0.5), ylim=c(0,1),
+          type='l', lwd=2.5,
+          col="deepskyblue")
+     text(0.0005, 0.95, paste( "AUSFC: ", round(ausfc_zoops, digits=4) ))
+     text(0.0005, 0.85, paste( "Frac. Occ: ", round(occ_frac, digits=4) ))
+     dev.off()
+  }
   
-  
-  resultString = paste(c(prefix, motifNumber, ausfc_zoops*occ_frac, occ_frac), collapse="\t")
+  if( args$mops != 'False' ){
+    ########### for mops model ###########
+    # read in p-values from the files
+    pvalues_mops <- unlist(read.table(paste(c(dir, "/", prefix, "_motif_", motifNumber, ".mops.pvalues"), collapse="") ))
+    
+    # avoid the rounding errors when p-value = 0 or p-value > 1
+    for(i in seq(1, length(pvalues_mops))){
+      if( pvalues_mops[i] > 1 ){
+        pvalues_mops[i] = 1
+      }
+    }
+    
+    # estimate False Discovery Rates for Diverse Test Statistics using fdrtool
+    result_mops = fdrtool( pvalues_mops, statistic="pvalue",
+                           plot=FALSE, color.figure=TRUE, verbose=TRUE )
+    
+    # get the global fdr values and estimate of the weight eta0 of
+    # the null component
+    fdr_mops <- result_mops$qval
+    eta0_mops <- result_mops$param[3]
+    
+    # avoid eta0 = 1
+    if(eta0_mops==1){
+      eta0_mops=0.9999999
+    }
+    
+    # calculate recall
+    len_mops = length(fdr_mops)
+    list_mops <- seq(1, len_mops )
+    recall_mops <- ( 1 - fdr_mops ) * list_mops / ( 1 - eta0_mops ) / len_mops
+    
+    # modify the SF curve:
+    # reset recall to 1 when it is larger than 1
+    for(i in list_mops){
+      if( recall_mops[i] > 1 ){
+        for(rest in i:len_mops){
+          recall_mops[rest] = 1
+        }
+        break
+      }
+    }
+    
+    # extend the SF curve till FDR reaches 0.5
+    recall_mops <- append(recall_mops, 1)
+    fdr_mops <- append(fdr_mops, 0.5)
+    list_mops <- seq(1, len_mops+1)
+    
+    # limit the frame of the curve
+    left_mops = 1
+    right_mops = len_mops+1
+    
+    for(i in list_mops){
+      if(fdr_mops[i] >= 0.0001 ){
+        left_mops = i
+        break
+      }
+    }
+    for(i in list_mops){
+      if(fdr_mops[i] >= 0.5 ){
+        right_mops = i
+        break
+      }
+    }
+    
+    list_mops <- seq(left_mops, right_mops)
+    
+    sum_area = log10(0.5) + 4
+    
+    # calculate motif occurrences per sequence
+    occ_mult = (1 - eta0_mops) * len_mops / len_zoops
+    
+    # compute  the area under the sensitivity-FDR curve(AUSFC):
+    if(right_mops == left_mops){
+      ausfc_mops = 0
+    } else {
+      ausfc_mops = sum(diff(log10(fdr_mops[list_mops]))*rollmean(recall_mops[list_mops],2)) / sum_area
+    }
+    if( args$plot != 'False' ){
+    # plot fdr vs. recall curve
+      png( file = paste( dir, "/", prefix, "_motif_", motifNumber, '_SFCurve_MOPS.png', sep = "" ) )
+      plot(fdr_mops[list_mops], recall_mops[list_mops], log="x",
+           main=paste(prefix, "_motif_", motifNumber, " Sensitivity vs. FDR for MOPS model", sep=""),
+           xlab="FDR", ylab="Sensitivity", xlim=c(0.0001,0.5), ylim=c(0,1),
+           type='l', lwd=2.5,
+           col="deepskyblue")
+      text(0.0005, 0.95, paste( "AUSFC: ", round(ausfc_mops, digits=4) ))
+      text(0.0005, 0.85, paste( "Mult. Occ.: ", round(occ_mult, digits=4) ))
+      dev.off()
+    }
+    
+  }
+  if( args$mops != 'False' ){
+    resultString = paste(c(prefix, motifNumber, ausfc_zoops*occ_frac, occ_frac, ausfc_mops*occ_mult, occ_mult), collapse="\t")
+  } else {
+    resultString = paste(c(prefix, motifNumber, ausfc_zoops*occ_frac, occ_frac), collapse="\t")
+  }
   print(resultString)
   results = c(results, resultString)
 }
