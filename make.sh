89273f1422f260d7683e59167ce278b94c948c40
Wanwan Ge <gewanwan92@gmail.com>
2017-06-21 13:38:14 +0200

    create make.sh for compiling BaMM

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# change compilation to clang:
+# http://google-engtools.blogspot.de/2011/05/c-at-google-here-be-dragons.html
+export CC=clang
+export CXX=clang++
+
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+# run automatic bugfinder for clang: http://clang-analyzer.llvm.org/
+# -B forces to build again... needed for the bug tracking
+scan-build make -B
+
+make -j 4
+make install
+
+rm -rf ~/opt/BaMM
+mkdir -p ~/opt/BaMM
+cp BaMMmotif/BaMMmotif ~/opt/BaMM
+cp ../R/plotAUSFC_benchmark_fdrtool.R ../R/plotAUSFC_rank.R ~/opt/BaMM
+
+cd ..
+
