58a1c5b64e3cb16e694b2102a9fe5f5ad7ec04be
Wanwan Ge <gewanwan92@gmail.com>
2017-11-14 14:58:39 +0100

    reduce example data size; install boost packages

--- src/refinement/mainBaMM.cpp
+++ src/refinement/mainBaMM.cpp
@@ -17,7 +17,7 @@ int main( int nargs, char* args[] ){
                   << "=  http://www.mpibpc.mpg.de/soeding  =" << std::endl
                   << "======================================" << std::endl;
     }
-    
+
 	// seed random number
 	srand( 42 );
 	Global::rngx.seed( 42 );
